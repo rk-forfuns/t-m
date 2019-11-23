653dc6d5235c8595986c83559d498faa20390895
Glitch (rasy-timestampmicroservice) <none>
2019-11-23 14:17:07 +0000

    👌🎊 Updated with Glitch 5

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const app = express();
-// const router = express.Router();
-
-const strftime = require("strftime");
-const timestampRouter = require('./timestamp');
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api/timestamp', timestampRouter);
 
 app.use(express.static("public"));
 
@@ -16,9 +11,25 @@ app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// app.get("/api/timestamp/", (req, res) => {
-//   res.json({ unix: Date.now(), utc: Date() });
-// });
+
+app.get("/api/timestamp",(req,res)=>{
+    var date = new Date();  
+    res.json({"unix":date.getTime(), 
+              "utc": date.toUTCString()})
+  
+});
+
+app.get("/api/timestamp/:date_string",(req,res)=>{
+  var date_string = req.params.date_string;
+  if ( String(parseInt(date_string))===date_string){ // Not date type
+    var date = new Date(parseInt(date_string)*1000);    
+  } else {
+    var date = new Date(date_string);  
+  }
+  res.json({"unix":date.getTime(), 
+            "utc": date.toUTCString()})
+  
+});
 
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
