fccbf593cabc6eac28b2af3489c8f35eed71b8b7
Glitch (rasy-timestampmicroservice) <none>
2019-11-23 12:50:32 +0000

    🚓👭 Updated with Glitch II

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const strftime = require('strftime');
+const strftime = require("strftime");
 
 app.use(express.json());
 
@@ -15,22 +15,26 @@ app.get("/api/timestamp/", (req, res) => {
 });
 
 app.get("/api/timestamp/:data", function(req, res) {
+  const { data } = req.params;
   let date = new Date();
+  if (!date.getTime()) {
+    return res.status(400).json({
+      error: "Invalid Date"
+    });
+  }
   if (/^\d*$/.test(req.params.data)) {
     date.setTime(req.params.data);
   } else {
     date = new Date(req.params.data);
   }
-  if (!date.getTime())
-    res.send(JSON.stringify({ error: "Invalid date given" }));
-  // else, we send the object with two members (unix and natural)
-  else
-    res.send(
-      JSON.stringify({
-        unix: date.getTime(),
-        natural: strftime("%B %d, %Y", date)
-      })
-    );
+  // if (!date.getTime())
+  //   res.send(JSON.stringify({ error: "Invalid date given" }));
+  // // else, we send the object with two members (unix and natural)
+  // else
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
